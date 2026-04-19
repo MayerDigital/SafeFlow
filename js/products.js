05c73f0c0c077b9c86f93ccd72868212800feaae
MayerDigital <christian.mayer@mayer-bauzeichnungen.de>
2026-04-19 11:29:04 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,8 +6,13 @@ export const products = [
     image: "assets/icons/brandschutz/feuerloescher.png",
     description: "Piktogramm Feuerlöscher",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
+    { label: "200 x 200 mm", price: 25,00 € }  
+    { label: "150 x 150 mm Fahneschild", price: 15,00 € },
+    { label: "200 x 200 mm Fahneschild", price: 20,00 € }
+    { label: "150 x 150 mm Nasenschild", price: 15,00 € },
+    { label: "200 x 200 mm Nasenschild", price: 20,0 € }
     ]
   },
   {
@@ -17,8 +22,13 @@ export const products = [
     image: "assets/icons/brandschutz/feuermelder.png",
     description: "Piktogramm Feuermelder",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
+    { label: "200 x 200 mm", price: 25,00 € }
+    { label: "150 x 150 mm Fahneschild", price: 15,00 € },
+    { label: "200 x 200 mm Fahneschild", price: 20,00 € }
+    { label: "150 x 150 mm Nasenschild", price: 15,00 € },
+    { label: "200 x 200 mm Nasenschild", price: 20,0 € }
     ]
   },
   {
@@ -28,8 +38,8 @@ export const products = [
     image: "assets/icons/brandschutz/hausmelder.png",
     description: "Piktogramm Hausmelder",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -39,8 +49,8 @@ export const products = [
     image: "assets/icons/brandschutz/loeschmittel.png",
     description: "Piktogramm Löschmittel",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -50,8 +60,13 @@ export const products = [
     image: "assets/icons/brandschutz/loeschschlauch.png",
     description: "Piktogramm Löschschlauch",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
+    { label: "200 x 200 mm", price: 25,00 € }
+    { label: "150 x 150 mm Fahneschild", price: 15,00 € },
+    { label: "200 x 200 mm Fahneschild", price: 20,00 € }
+    { label: "150 x 150 mm Nasenschild", price: 15,00 € },
+    { label: "200 x 200 mm Nasenschild", price: 20,0 € }
     ]
   },
   {
@@ -61,8 +76,8 @@ export const products = [
     image: "assets/icons/brandschutz/loeschdecke.png",
     description: "Piktogramm Löschdecke",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -72,8 +87,8 @@ export const products = [
     image: "assets/icons/brandschutz/nottelefon.png",
     description: "Piktogramm Nottelefon",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -83,8 +98,8 @@ export const products = [
     image: "assets/icons/brandschutz/roterpfeillinks.png",
     description: "Piktogramm Roter Pfeil links",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -94,8 +109,8 @@ export const products = [
     image: "assets/icons/brandschutz/roterpfeiloben.png",
     description: "Piktogramm Roter Pfeil oben",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -105,8 +120,8 @@ export const products = [
     image: "assets/icons/brandschutz/roterpfeilrechts.png",
     description: "Piktogramm Roter Pfeil rechts",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -116,8 +131,8 @@ export const products = [
     image: "assets/icons/brandschutz/roterpfeilunten.png",
     description: "Piktogramm Roter Pfeil unten",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -127,8 +142,8 @@ export const products = [
     image: "assets/icons/rettung/arzt.png",
     description: "Piktogramm Arzt",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -138,8 +153,8 @@ export const products = [
     image: "assets/icons/rettung/augenspuelung.png",
     description: "Piktogramm Augenspülung",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -149,8 +164,12 @@ export const products = [
     image: "assets/icons/rettung/defibrillator.png",
     description: "Piktogramm Defibrillator",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
+    { label: "150 x 150 mm Fahneschild", price: 15,00 € },
+    { label: "200 x 200 mm Fahneschild", price: 20,00 € }
+    { label: "150 x 150 mm Nasenschild", price: 15,00 € },
+    { label: "200 x 200 mm Nasenschild", price: 20,0 € }
     ]
   },
   {
@@ -160,8 +179,12 @@ export const products = [
     image: "assets/icons/rettung/erstehilfe.png",
     description: "Piktogramm Erste Hilfe",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
+    { label: "150 x 150 mm Fahneschild", price: 15,00 € },
+    { label: "200 x 200 mm Fahneschild", price: 20,00 € }
+    { label: "150 x 150 mm Nasenschild", price: 15,00 € },
+    { label: "200 x 200 mm Nasenschild", price: 20,0 € }
     ]
   },
   {
@@ -171,8 +194,8 @@ export const products = [
     image: "assets/icons/rettung/evakuierungsstuhl.png",
     description: "Piktogramm Evakuierungsstuhl",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -182,8 +205,8 @@ export const products = [
     image: "assets/icons/rettung/notausganglinks.png",
     description: "Piktogramm Notausgang links",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -193,8 +216,8 @@ export const products = [
     image: "assets/icons/rettung/notausgangrechts.png",
     description: "Piktogramm Notausgang rechts",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -204,8 +227,7 @@ export const products = [
     image: "assets/icons/rettung/notausstieg.png",
     description: "Piktogramm Notausstieg",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -215,8 +237,7 @@ export const products = [
     image: "assets/icons/rettung/notausstiegleiter.png",
     description: "Piktogramm Notausstieg mit Leiter",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+      { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -226,8 +247,8 @@ export const products = [
     image: "assets/icons/rettung/notdusche.png",
     description: "Piktogramm Notdusche",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
   {
@@ -237,8 +258,8 @@ export const products = [
     image: "assets/icons/rettung/notliege.png",
     description: "Piktogramm Notliege",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+    { label: "150 x 150 mm", price: 10,00 € },
+    { label: "200 x 200 mm", price: 12,50 € }
     ]
   },
  
@@ -249,8 +270,7 @@ export const products = [
     image: "assets/icons/rettung/sammelplatz.png",
     description: "Piktogramm Sammelplatz",
     sizes: [
-      { label: "150 x 150 mm", price: 0 },
-      { label: "200 x 200 mm", price: 0 }
+      { label: "400 x 400 mm", price: 30,00 € }
     ]
   }
 ];
